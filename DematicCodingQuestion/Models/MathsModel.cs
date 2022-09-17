72f265eb32ddb6fd79ebda6b9cf02716afcabcb6
Ritcus <adonism9999@gmail.com>
2022-09-18 09:05:29 +1000

    Add project files.

---
+++ DematicCodingQuestion/Models/MathsModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace DematicCodingQuestion.Models
+{
+    [XmlRoot("Maths")]
+    public class Operations
+    {
+        [XmlElement("Operation ID")]
+
+        public string OperationId { get; set; }
+
+        [XmlElement("Value")]
+        public List<double> Value { get; set; }
+
+    }
+}
---
+++ DematicCodingQuestion/Models/MyMaths.cs
@@ -0,0 +1,29 @@
+﻿using System.Xml.Serialization;
+
+public enum Operator
+
+{
+
+    Addition,
+
+    Subtraction,
+
+    Multiplication,
+
+    Division
+
+}
+
+
+[XmlRoot("Maths")]
+public class Operation
+
+{
+    [XmlElement("Value")]
+    public List<double> Value { get; set; }
+
+
+    [XmlElement("Operation ID")]
+    public Operator ID { get; set; }
+
+}
